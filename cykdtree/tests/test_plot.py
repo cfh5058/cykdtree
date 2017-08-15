920ffc2f13e6ffbc94c1fdecf21a12d8814da714
langmm <cfh5058@gmail.com>
2017-08-15 17:01:54 +0000

    Testing of multiple sets of points in serial

--- cykdtree/tests/test_plot.py
+++ cykdtree/tests/test_plot.py
@@ -11,12 +11,12 @@ from cykdtree.tests import make_points
 
 def test_plot2D_serial():
     fname_test = "test_plot2D_serial.png"
-    pts, le, re, ls = make_points(100, 2)
+    pts, le, re, ls = make_points(10, 2, leafsize=2)
     tree = PyKDTree(pts, le, re, leafsize=ls)
     axs = plot2D_serial(tree, pts, title="Serial Test", plotfile=fname_test,
                         label_boxes=True)
+    plot2D_serial(tree, [pts, pts], axs=axs, plotfile=fname_test)
     os.remove(fname_test)
-    # plot2D_serial(tree, pts, axs=axs)
     del axs
 
 
