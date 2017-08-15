ed09e1ffb40fd4ec19c2469ee3f81a03cef94c82
langmm <cfh5058@gmail.com>
2017-08-15 16:53:49 +0000

    Added testing for backwards compatibility

---
+++ cykdtree/tests/test_backwards.py
@@ -0,0 +1,14 @@
+import os
+from nose.tools import assert_raises, eq_
+from cykdtree import backwards
+
+def test_pickle():
+    test_obj = {'test': 0}
+    ftest = 'test_pickle.dat'
+    if os.path.isfile(ftest):
+        os.remove(ftest)
+    assert_raises(AssertionError, backwards.load_from_pickle, ftest)
+    backwards.dump_to_pickle(ftest, test_obj)
+    res = backwards.load_from_pickle(ftest)
+    eq_(res, test_obj)
+    os.remove(ftest)
