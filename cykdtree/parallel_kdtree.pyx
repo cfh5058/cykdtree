03ebda293d0124d92a218d33a3ab346d54715b66
langmm <cfh5058@gmail.com>
2017-08-15 03:37:08 +0000

    Debugging pstats in parallel

--- cykdtree/parallel_kdtree.pyx
+++ cykdtree/parallel_kdtree.pyx
@@ -119,6 +119,7 @@ def parallel_worker(finput, foutput):
         if isinstance(profile, str):
             pr.dump_stats(profile)
         else:
+            pr.print_stats(25)
             print(dir(pr))
             ps = pstats.Stats(pr)
             ps.sort_stats('time').print_stats(25)
